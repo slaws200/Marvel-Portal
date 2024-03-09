f98c9c566da87babeda4318b1d2df7b71360808a
Your Name <you@example.com>
2024-03-09 16:42:47 +0200

    load more characters added

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,8 +1,8 @@
 
-
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=2a595b5cce58a11818ab35614802e7b4';
+    _baseOffset = 0;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -14,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=300&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
